717e3f1486a9f83084798bb6244e6bdeaa99363c
Roman Zherebko <rubilnik89@mail.ru>
2016-06-02 18:12:44 +0300

    perenos v index

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -230,36 +230,7 @@ class ArticleController extends Controller
 
     public function index(Request $request){
 
-        $city = $request->query('city');
-        $startDate = $request->query('startDate');
-        $endDate = $request->query('endDate');
-        $within = $request->query('within');
-        $category = $request->query('category');
-        $longitude = $request->query('longitude');
-        $latitude = $request->query('latitude');
-        $page_number = $request->query('page_number');
 
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'https://www.eventbriteapi.com/v3/events/search/' , [
-            'query'=>[
-                'location.address' => $city,
-                'start_date.range_start' => $startDate,
-                'start_date.range_end' => $endDate,
-                'location.within' => $within,
-                'location.latitude' => $latitude,
-                'location.longitude' => $longitude,
-                'categories' => $category,
-                'page' => $page_number,
-                'expand'=>'category,venue,ticket_classes',
-                'token'=>'OSUKWVGNBKZJWDBWKBIC',],
-            'verify' => false,
-            // todo: move token in .env
-            //    'token'=>'OSUKWVGNBKZJWDBWKBIC',
-        ]);
-
-        $body = $response->getBody();
-        //   $evenz = json_decode($body);
-        return $body;
 
     }
 
